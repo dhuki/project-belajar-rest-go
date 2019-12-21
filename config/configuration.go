c0df5086f9119ce2e3647fa9f33e9454cb9e3b69
dhuki <dhukidwir@gmail.com>
2019-12-21 10:11:34 +0700

    perbaikan configuration

--- config/configuration.go
+++ config/configuration.go
@@ -28,10 +28,10 @@ func (s *Server) Initialize() {
 	// dbName := os.Getenv("db.name")
 	// dbHost := os.Getenv("db.host")
 
-	username := "postgres"
-	password := "rahasia"
-	dbName := "template_db"
-	dbHost := "localhost"
+	username := "ijjfgiyjcbrznd"
+	password := "028ccc705a477aabf05483a69471b3cd349122d598495748014d446c7aad41dd"
+	dbName := "d4ppbeeimehina"
+	dbHost := "ec2-174-129-255-21.compute-1.amazonaws.com"
 
 	//(format without printing)
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
