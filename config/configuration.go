9cfdce3d7f712f6d3863e8b4098a72d44bf00d26
dhuki <dhukidwir@gmail.com>
2019-12-21 11:08:35 +0700

    perbaikan port

--- config/configuration.go
+++ config/configuration.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -60,9 +61,18 @@ func (s *Server) SetupServer() {
 }
 
 func (s *Server) Run() {
-	port := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(GetPort(), s.Router))
+}
 
-	log.Fatal(http.ListenAndServe(port, s.Router))
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
 
 func MainServerStart() {
